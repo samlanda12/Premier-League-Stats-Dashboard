42db2cd7a746b4f035b2696fd4aae879ea921f9d
samlanda12 <sammlannda@gmail.com>
2025-07-22 12:39:02 -0400

    Configure Flask app to run on specified host and port

--- app.py
+++ app.py
@@ -12,4 +12,6 @@ app.config['LEAGUE'] = league  # store league instance in app config
 app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
